8143f9396c8b6c204ae0e7db51576e2dba89eff9
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-04-09 11:54:21 +0000

    progetto quasi finito

--- Esercizio_9/src/app/app.component.ts
+++ Esercizio_9/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { IntroductionComponent } from './introduction/introduction.component';
 import { ListComponent } from './list/list.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IntroductionComponent, ListComponent, NavigationComponent],
+  imports: [RouterOutlet, IntroductionComponent, ListComponent, NavigationComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
