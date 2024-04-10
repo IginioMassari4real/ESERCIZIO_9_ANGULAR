14851713feaf056d70f21f080f5b6a1f37766d28
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-04-10 21:46:03 +0000

    progetto finito

--- Esercizio_9/src/app/list/list.component.ts
+++ Esercizio_9/src/app/list/list.component.ts
@@ -12,15 +12,15 @@ export class ListComponent {
 
 viaggi=[
 
- {destinazione:"Mercurio", appartenenza:"Sistema Solare", durata:"7 mesi", partenza:"Terra (Kennedy Space Center (KSC) - Situato a Cape Canaveral, in Florida)", datapartenza:"01/06/2024", dataarrivo:"01/'01/2025", costo:"140000", immagine:"https://www.focus.it/site_stored/imgs/0005/005/me4.1020x680.jpg"},
+ {destinazione:"Mercurio", appartenenza:"Sistema Solare", durata:"7 mesi", partenza:"Terra (Kennedy Space Center (KSC) - Situato a Cape Canaveral, in Florida)", datapartenza: new Date(2024, 5, 1), dataarrivo: new Date(2025, 0, 1), costo:"140000", immagine:"https://www.focus.it/site_stored/imgs/0005/005/me4.1020x680.jpg"},
 
- {destinazione:"Venere", appartenenza:"Sistema Solare", durata:"5 mesi", partenza:"Terra (Kennedy Space Center (KSC) - Situato a Cape Canaveral, in Florida)", datapartenza:"10/06/2024", dataarrivo:"10/11/2024", costo:"170000", immagine:"https://www.nationalgeographic.it/upload/ngi-hero/pia00271_detail.jpg"},
+ {destinazione:"Venere", appartenenza:"Sistema Solare", durata:"5 mesi", partenza:"Terra (Kennedy Space Center (KSC) - Situato a Cape Canaveral, in Florida)", datapartenza: new Date(2024, 5, 10), dataarrivo: new Date(2024, 10, 10), costo:"170000", immagine:"https://www.nationalgeographic.it/upload/ngi-hero/pia00271_detail.jpg"},
 
- {destinazione:"Marte", appartenenza:"Sistema Solare", durata:"9 mesi", partenza:"Terra (Kennedy Space Center (KSC) - Situato a Cape Canaveral, in Florida)", datapartenza:"15/06/2024", dataarrivo:"15/03/2025", costo:"155000", immagine:"https://www.nationalgeographic.it/upload/ngi-hero/mars-globe-valles-marineris-enhanced.jpg"},
+ {destinazione:"Marte", appartenenza:"Sistema Solare", durata:"9 mesi", partenza:"Terra (Kennedy Space Center (KSC) - Situato a Cape Canaveral, in Florida)", datapartenza: new Date(2024, 5, 15), dataarrivo: new Date(2024, 2, 15), costo:"155000", immagine:"https://www.nationalgeographic.it/upload/ngi-hero/mars-globe-valles-marineris-enhanced.jpg"},
 
- {destinazione:"Saturno", appartenenza:"Sistema Solare", durata:"1 anno e mezzo", partenza:"Terra (Kennedy Space Center (KSC) - Situato a Cape Canaveral, in Florida)", datapartenza:"25/06/2024", dataarrivo:"25/12/2025", costo:"120000", immagine:"https://content.nationalgeographic.com.es/medio/2018/12/20/polo-norte-de-saturno-y-sus-anillos_88724cae_1280x650.jpg"},
+ {destinazione:"Saturno", appartenenza:"Sistema Solare", durata:"1 anno e mezzo", partenza:"Terra (Kennedy Space Center (KSC) - Situato a Cape Canaveral, in Florida)", datapartenza: new Date(2024, 5, 25), dataarrivo: new Date(2025, 11, 25), costo:"120000", immagine:"https://content.nationalgeographic.com.es/medio/2018/12/20/polo-norte-de-saturno-y-sus-anillos_88724cae_1280x650.jpg"},
 
- {destinazione:"Giove", appartenenza:"Sistema Solare", durata:"2 anni e mezzo", partenza:"Terra (Kennedy Space Center (KSC) - Situato a Cape Canaveral, in Florida)", datapartenza:"17/06/2024", dataarrivo:"17/12/2026", costo:"133000", immagine:"https://staticfanpage.akamaized.net/wp-content/uploads/sites/5/2018/01/giove-cover.jpg"},
+ {destinazione:"Giove", appartenenza:"Sistema Solare", durata:"2 anni e mezzo", partenza:"Terra (Kennedy Space Center (KSC) - Situato a Cape Canaveral, in Florida)", datapartenza: new Date(2024, 5, 17), dataarrivo: new Date(2026, 11, 17), costo:"133000", immagine:"https://staticfanpage.akamaized.net/wp-content/uploads/sites/5/2018/01/giove-cover.jpg"},
 
 ]
 
